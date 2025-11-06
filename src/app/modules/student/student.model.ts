47f7b9810ab201acc7c11dd87804272102867d11
Abdul Al Roman <mdroman45678910@gmail.com>
2025-11-07 01:35:18 +0600

    feat(student delete): delete student chack using findone and aggreatetion pippeline

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -64,9 +64,19 @@ studentSchema.pre('save', async function (next) {
 // Quiery Middelweare
 studentSchema.pre('find', function (next) {
   this.find({ isDeleted: { $ne: true } })
-
   next()
 })
+
+studentSchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } })
+  next()
+})
+
+studentSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
 
 
 // pre hook midddeller
