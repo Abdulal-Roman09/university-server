1d795b08cab601977aad1973e90d01a06801119c
Abdul Al Roman <mdroman45678910@gmail.com>
2025-11-14 03:54:25 +0600

    feat(mongooes Validation):add mongoes validation erro

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,43 +1,44 @@
 import { ErrorRequestHandler } from "express";
 import { TErrorSources } from "../interface/error";
 import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
 import config from "../config";
 import { ZodError } from "zod";
 
-// Global Error Handler Middleware
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-
   let statusCode = 500;
   let message = "Something went wrong";
-  let errorSources: TErrorSources = [{
-    path: '',
-    message: 'Something went Wrong'
-  }
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
 
-  ]
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources
+
+
+
+
+
   }
 
   return res.status(statusCode).json({
     success: false,
     message,
     errorSources,
-    err,
-    stack: config.NODE_ENV === 'development' ? err?.stack : null
+    err: config.NODE_ENV === 'development' ? err : undefined,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
 export default globalErrorHandler;
-/*
-success
-message
-errorSources:[
-  path:'',
-  message:''
-]
-stack
-*/
