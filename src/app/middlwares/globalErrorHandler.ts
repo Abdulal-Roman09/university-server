19611999c9e0bd6d00af7c2347efc4a8788d14f2
Abdul Al Roman <mdroman45678910@gmail.com>
2025-11-14 03:45:50 +0600

    feat(zod validation custom):add custom zod validation erroro

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -1,20 +1,43 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from "express";
+import { TErrorSources } from "../interface/error";
+import handleZodError from "../errors/handleZodError";
+import config from "../config";
+import { ZodError } from "zod";
 
 // Global Error Handler Middleware
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Something went wrong";
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+
+  let statusCode = 500;
+  let message = "Something went wrong";
+  let errorSources: TErrorSources = [{
+    path: '',
+    message: 'Something went Wrong'
+  }
+
+  ]
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null
   });
 };
 
 export default globalErrorHandler;
+/*
+success
+message
+errorSources:[
+  path:'',
+  message:''
+]
+stack
+*/
