9af822d493662483d35409d0977d461bd99e71d1
Abdul Al Roman <mdroman45678910@gmail.com>
2025-11-15 16:07:23 +0600

    feat(filter):apply filter using querybuilder

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,30 +1,42 @@
 import { Query, FilterQuery } from "mongoose";
+import { query } from 'express';
 
 class QueryBuilder<T> {
-  public modelQuery: Query<T[], T>;
-  public query: Record<string, unknown>;
+    public modelQuery: Query<T[], T>;
+    public query: Record<string, unknown>;
 
-  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
-    this.modelQuery = modelQuery;
-    this.query = query;
-  }
-
-  search(searchableFields: string[]) {
-    const searchTerm = this?.query?.searchTerm as string;
-
-    if (searchTerm) {
-      this.modelQuery = this.modelQuery.find({
-        $or: searchableFields.map(
-          (field) =>
-            ({
-              [field]: { $regex: searchTerm, $options: "i" },
-            }) as FilterQuery<T>
-        ),
-      });
+    constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+        this.modelQuery = modelQuery;
+        this.query = query;
     }
 
-    return this;
-  }
+    search(searchableFields: string[]) {
+        const searchTerm = this?.query?.searchTerm as string;
+
+        if (searchTerm) {
+            this.modelQuery = this.modelQuery.find({
+                $or: searchableFields.map(
+                    (field) =>
+                        ({
+                            [field]: { $regex: searchTerm, $options: "i" },
+                        }) as FilterQuery<T>
+                ),
+            });
+        }
+
+        return this;
+    }
+
+    filter() {
+        const queryObj = { ...this.query }
+        const excludeFildes = ['serachTerm', 'sort', 'limit', 'page', 'fields']
+        excludeFildes.forEach((element) => delete queryObj[element])
+        this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>)
+        return this
+    }
+  
+
 }
+
 
 export default QueryBuilder;
