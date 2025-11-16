c85e28bd15a6177ed296c0c2230f2484ab5cefa8
Abdul Al Roman <mdroman45678910@gmail.com>
2025-11-17 00:40:24 +0600

    feat(auth):chack user in differnt stage in differnt setion

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,10 +1,37 @@
+
+import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
+import httpStatus from 'http-status';
+import bcrypt from 'bcryptjs';
+import AppError from "../../errors/AppError.";
+
 
 
 const loginUser = async (payload: TLoginUser) => {
-    console.log(payload)
-}
+    // Check if user exists
+    const user = await User.findOne({ id: payload.id });
+    if (!user) {
+        // 404 Not Found – user not found
+        throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
+    }
+
+    // Check if user is deleted
+    if (user.isDeleted) {
+        // 410 Gone – resource deleted
+        throw new AppError(httpStatus.GONE, 'This user is deleted');
+    }
+
+    // Check if user is blocked
+    if (user.status === 'blocked') {
+        // 403 Forbidden – user blocked
+        throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked');
+    }
+
+    // Chack the password is correncted
+    const isPasswordMatched =await bcrypt.compare(payload?.password, user?.password)
+
+};
 
 export const AuthServices = {
     loginUser
-}
+};
